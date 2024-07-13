4c18cdd53ad91afe114cb15ead31306de706141c
strmdctem <strmdctem@gmail.com>
2024-07-13 22:54:54 +0530

    updated

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => {
-  console.log('Service worker installed');
-});
-self.addEventListener('fetch', (event) => {
-  console.log('Service worker fetched a request');
-  event.respondWith(fetch(event.request));
-});
